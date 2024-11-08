d7cf76ac7c5aa13e1b0d8c7252c4b52433a4c49e
Alexander Smith <hongzeyuan0@gmail.com>
2024-11-08 21:49:55 +0800

    设置mime类型

--- http/code/07-静态资源服务/server.js
+++ http/code/07-静态资源服务/server.js
@@ -2,6 +2,29 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
+const mimes = {
+  css: 'text/css',
+  js: 'text/javascript',
+  html: 'text/html',
+  json: 'application/json',
+  png: 'image/png',
+  jpg: 'image/jpeg',
+  jpeg: 'image/jpeg',
+  gif: 'image/gif',
+  svg: 'image/svg+xml',
+  ico: 'image/x-icon',
+  mp3: 'audio/mpeg',
+  mp4: 'video/mp4',
+  pdf: 'application/pdf',
+  doc: 'application/msword',
+  xls: 'application/vnd.ms-excel',
+  ppt: 'application/vnd.ms-powerpoint',
+  woff: 'application/font-woff',
+  ttf: 'application/font-sfnt',
+  eot: 'application/vnd.ms-fontobject',
+  otf: 'application/font-sfnt',
+}
+
 const server = http.createServer((req, res) => {
   // const htmlPath = path.join(__dirname, '../资料/table.html');
   // const cssPath = path.join(__dirname, '../资料/table.css');
@@ -10,6 +33,10 @@ const server = http.createServer((req, res) => {
   const filePath = path.join(__dirname, 'page', pathname);
   console.log('filePath', filePath);
   fs.readFile(filePath, 'utf8', (err, data) => {
+    const ext = path.extname(filePath).slice(1);
+    const type = mimes[ ext ] || 'application/octet-stream';
+    console.log('type', type);
+    res.setHeader('Content-Type', `${type}; charset=utf-8`);
     if (err) {
       res.setStatusCode = 404;
       res.end('<h1>404 Not Found</h1>');
